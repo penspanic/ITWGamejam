5dda35a54592cca158d77289ca07a5276c8a75da
penspanic <penspanic@gmail.com>
2017-04-23 11:25:33 +0900

    [박근희] 아이템 클래스 잘못된 것 수정

--- ITWClient/Assets/Scripts/Logic/Item/IItem.cs
+++ ITWClient/Assets/Scripts/Logic/Item/IItem.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using UnityEngine;
 
-namespace Assets.Scripts.Logic.Item
+public abstract class IItem : MonoBehaviour
 {
-    class IItem
+    protected virtual void Awake()
     {
+
     }
 }
