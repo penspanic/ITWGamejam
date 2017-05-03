bb6ed9fc81b17fd3371bb148da2ec9c6f1029d97
penspanic <penspanic@gmail.com>
2017-05-03 15:41:10 +0900

    [박근희] 이팩트 적용 전 작업

---
+++ ITWClient/Assets/Scripts/Logic/Controller/EffectController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum EffectType
+{
+    Hit,
+}
+
+public class EffectController : MonoBehaviour
+{
+    private Dictionary<EffectType, GameObject> effectPrefabs = new Dictionary<EffectType, GameObject>();
+    private void Awake()
+    {
+        effectPrefabs.Add(EffectType.Hit, Resources.Load<GameObject>("Prefabs/Effect/Hit"));
+    }
+
+    public void ShowEffect(EffectType type, Vector2 pos, Transform parent = null)
+    {
+        GameObject newEffect = Instantiate(effectPrefabs[type]);
+        if(parent != null)
+            newEffect.transform.SetParent(parent);
+        newEffect.transform.localPosition = pos;
+    }
+}
