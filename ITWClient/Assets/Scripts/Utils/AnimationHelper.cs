fec0f2ad658ca7b4ea28ec127929e9c4395026f9
penspanic <penspanic@gmail.com>
2017-05-03 11:40:33 +0900

    [박근희] 헤비 스킬 버그 수정(포물선으로 움직이는거)

--- ITWClient/Assets/Scripts/Utils/AnimationHelper.cs
+++ ITWClient/Assets/Scripts/Utils/AnimationHelper.cs
@@ -2,28 +2,43 @@
 
 public static class AnimationHelper
 {
-    public static Vector2 Arc(Vector2 start, Vector2 end, float max, float time)
+    public static Vector2 Arc(Vector2 start, Vector2 end, float ratio, float time)
     {
-        float added = Mathf.Sin(time * Mathf.PI);
+        float added = Mathf.Sin(time * Mathf.PI) * ratio;
         Vector2 current = EasingUtil.EaseVector2(EasingUtil.linear, start, end, time);
         Vector2 dirVec = (end - start).normalized;
         Vector2 up = new Vector2();
-        if(dirVec.x < 0 && dirVec.y > 0)
+        if(dirVec.x < 0)
         {
-            up = Quaternion.AngleAxis(90, dirVec).eulerAngles.normalized;
+            //up = Quaternion.AngleAxis(90, dirVec).eulerAngles.normalized;
+            up = (Quaternion.Euler(0, 0, -90) * dirVec).normalized;
+            //up = Quaternion.AngleAxis(90, dirVec).eulerAngles.normalized;
         }
-        else if(dirVec.x >= 0 && dirVec.y > 0)
+        else
         {
-            up = Quaternion.AngleAxis(-90, dirVec).eulerAngles.normalized;
+            //up = Quaternion.AngleAxis(-90, dirVec).eulerAngles.normalized;
+            up = (Quaternion.Euler(0, 0, 90) * dirVec).normalized;
+            //up = Quaternion.AngleAxis(-90, dirVec).eulerAngles.normalized;
         }
-        else if(dirVec.x < 0 && dirVec.y <= 0)
-        {
-            up = Quaternion.AngleAxis(90, dirVec).eulerAngles.normalized;
-        }
-        else // dirVec.x >= 0 && dirVec.y <= 0)
-        {
-            up = Quaternion.AngleAxis(-90, dirVec).eulerAngles.normalized;
-        }
+
+        //if(dirVec.x < 0 && dirVec.y > 0)
+        //{
+        //    up = Quaternion.AngleAxis(90, dirVec).eulerAngles.normalized;
+        //}
+        //else if(dirVec.x >= 0 && dirVec.y > 0)
+        //{
+        //    up = Quaternion.AngleAxis(-90, dirVec).eulerAngles.normalized;
+        //}
+        //else if(dirVec.x < 0 && dirVec.y <= 0)
+        //{
+        //    up = Quaternion.AngleAxis(90, dirVec).eulerAngles.normalized;
+        //}
+        //else // dirVec.x >= 0 && dirVec.y <= 0)
+        //{
+        //    up = Quaternion.AngleAxis(-90, dirVec).eulerAngles.normalized;
+        //}
+        Debug.Log("dir vec : " + dirVec + " up : " + up);
+        Debug.Log(up * added);
         current += up * added;
         return current;
     }
