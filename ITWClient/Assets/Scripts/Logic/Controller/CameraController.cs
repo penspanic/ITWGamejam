f649152e41289ed0de79ed403b213af85c3ff9f7
penspanic <penspanic@gmail.com>
2017-05-05 01:01:04 +0900

    [박근희] 카메라 뭔가 수정함

--- ITWClient/Assets/Scripts/Logic/Controller/CameraController.cs
+++ ITWClient/Assets/Scripts/Logic/Controller/CameraController.cs
@@ -9,7 +9,7 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     private GameObject[] targets;
     [SerializeField]
-    private float minOrthoSize;
+    private Vector2 orthoSizeRange;
     private void Awake()
     {
 
@@ -31,6 +31,7 @@ public class CameraController : MonoBehaviour
         // http://www.gamasutra.com/blogs/ItayKeren/20150511/243083/Scroll_Back_The_Theory_and_Practice_of_Cameras_in_SideScrollers.php
         while(true)
         {
+            yield return new WaitForFixedUpdate();
             Rect targetRect = new Rect();
             foreach(GameObject target in targets)
             {
@@ -44,17 +45,29 @@ public class CameraController : MonoBehaviour
                 if(pos.y > targetRect.yMax)
                     targetRect.yMax = pos.y;
             }
+            float size = 0f;
+            if(targetRect.height > targetRect.width)
+            {
+                size = targetRect.height;
+                size += 0.3f; // 하단 StatusBox 땜에
+            }
+            else
+            {
+                size = targetRect.width;
+                size /= 1.777777777777f;
+            }
+
+            float orthoSize = size / 2f + 0.3f;
+            orthoSize = Mathf.Lerp(Camera.main.orthographicSize, orthoSize, Time.deltaTime * 2f);
+            if(orthoSize < orthoSizeRange.x)
+                orthoSize = orthoSizeRange.x;
+            else if(orthoSize > orthoSizeRange.y)
+                orthoSize = orthoSizeRange.y;
+            Camera.main.orthographicSize = orthoSize;
             Vector3 newPos = Camera.main.transform.position;
             newPos.x = targetRect.center.x;
             newPos.y = targetRect.center.y;
             Camera.main.transform.position = newPos;
-            float orthoSize = targetRect.height / 2f + 0.8f;
-            orthoSize = Mathf.Lerp(Camera.main.orthographicSize, orthoSize, Time.deltaTime);
-            if(orthoSize < minOrthoSize)
-                orthoSize = minOrthoSize;
-            Camera.main.orthographicSize = orthoSize;
-
-            yield return new WaitForFixedUpdate();
         }
     }
 }
