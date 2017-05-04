755c07bb12a8ae3a48ee2becff32221387b11345
penspanic <penspanic@gmail.com>
2017-05-05 00:01:47 +0900

    [박근희] 피격 색깔, 깜빡이는거 작업

---
+++ ITWClient/Assets/Scripts/Utils/SpriteAnimator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteAnimator : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer[] targetRenderers = null;
+
+    public void SetColor(Color color)
+    {
+        for(int i = 0; i < targetRenderers.Length; ++i)
+        {
+            targetRenderers[i].color = color;
+        }
+    }
+
+    public IEnumerator Twinkle(float time, float speed)
+    {
+        float elapsedTime = 0f;
+        bool isRendererOn = false;
+        while(elapsedTime < time)
+        {
+            for(int i = 0; i < targetRenderers.Length; ++i)
+            {
+                targetRenderers[i].enabled = isRendererOn;
+            }
+            isRendererOn = !isRendererOn;
+            yield return new WaitForSeconds(speed / 2f);
+            elapsedTime += speed / 2f;
+        }
+        for(int i = 0; i < targetRenderers.Length;++i)
+        {
+            targetRenderers[i].enabled = true;
+        }
+    }
+}
