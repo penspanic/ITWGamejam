3e1959cfda5c888d1f051156d7501eaafe6f8236
penspanic <penspanic@gmail.com>
2017-07-17 23:57:46 +0900

    [박근희] 인게임 옵션 MasterVolume 기능 적용

--- ITWClient/Assets/Scripts/Ui/InGame/InGameOption.cs
+++ ITWClient/Assets/Scripts/Ui/InGame/InGameOption.cs
@@ -8,12 +8,15 @@ public class InGameOption : MonoBehaviour
     public bool IsShowing { get; private set; }
 
     [SerializeField]
+    private Slider masterVolumeSlider;
+    [SerializeField]
     private GuidePanel guidePanel;
 
     public void Show()
     {
         Time.timeScale = 0f;
         IsShowing = true;
+        masterVolumeSlider.value = SoundManager.MasterVolume;
         this.gameObject.SetActive(true);
     }
 
@@ -23,6 +26,11 @@ public class InGameOption : MonoBehaviour
         IsShowing = false;
         guidePanel.Hide();
         this.gameObject.SetActive(false);
+    }
+
+    public void OnMasterVolumeSliderChanged()
+    {
+        SoundManager.MasterVolume = masterVolumeSlider.value;
     }
 
     public void OnGuideButtonDown()
